0d2a5c8999a4655721236a7b92074e7a864c76f0
caiquemiranda <caiquemirandas@gmail.com>
2025-07-02 21:25:02 -0300

    style css page titulo

--- react-fundamentos/src/app/componente/titulo/page.tsx
+++ react-fundamentos/src/app/componente/titulo/page.tsx
@@ -1,8 +1,8 @@
 export default function Page(){
     return (
         <div>
-            <h1 className="text-amber-200">Titulo</h1>
-            <h2>Subtitulo</h2>
+            <h1 className="text-amber-200 text-8xl font-bold">Titulo</h1>
+            <h2 className="text-emerald-600 text-3xl">Subtitulo</h2>
         </div>
     )
 }
